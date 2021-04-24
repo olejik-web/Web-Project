071e90e659c0e52ce0e4c16cf9ba8812eefe5ba6
olegator2004 <59178029+olejik-web@users.noreply.github.com>
2021-04-24 22:36:54 +0300

    commit-10

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from flask_wtf.file import FileField, FileRequired
 from werkzeug.utils import secure_filename
 from PIL import Image
 import os
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -22,14 +23,19 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+class PartForm(FlaskForm):
+    header = StringField('Название раздела',
+                        validators=[DataRequired()])
+    imgs = 
+
+
 class CreatePageForm(FlaskForm):
     header = StringField('Название страницы',
                         validators=[DataRequired()])
     about = TextAreaField("Краткая информация о странице", 
                           validators=[DataRequired()])
-    photos = FileField('Прикрепите изображения, используемые на странице', 
-                       validators=[FileRequired()])
-    content = TextAreaField("Содержание страницы", validators=[DataRequired()])
+    parts = FieldList(FormField(PartForm))
+    submit = SubmitField('Сохранить')
 
 
 class LoginForm(FlaskForm):
@@ -60,6 +66,43 @@ def index():
     return render_template('index.html', title='Главная')
 
 
+@app.route('/edit_page/<page_info>')
+@login_required
+def edit_page(page_info):
+    db_sess = db_session.create_session()
+    form = CreatePageForm()
+    page_id = int(page_info.split('$')[1])
+    page_type = page_info.split('$')[0]
+    page = db_sess.query(Page).filter(Page.id == page_id).first()
+    if page_type == 'page_of_book':
+        form = CreatePageForm()
+        db_sess = db_session.create_session()
+        page = db_sess.query(Page).filter(Page.id == page_id).first()
+        form.header.data = page.header
+        form.about.data = page.about
+        with open(page.json_page) as file:
+            page_json = json.load(file)
+        form.content = page_json['content']
+        for elem in form.content:
+            elem['header'] = StringField('123',
+                        validators=[DataRequired()])
+            elem['imgs'] = (FileField('Прикрепите изображения к статье', 
+                       validators=[FileRequired()]), elem['imgs'])
+            elem['content'] = TextAreaField("Текст раздела", 
+                          validators=[DataRequired()])
+        print(StringField('123',
+                        validators=[DataRequired()]))
+        print(form.content[0]['header'])
+        print(form.header)
+        
+        if form.validate_on_submit():
+            return redirect('/users')
+        else:
+            return render_template('edit_page_of_book.html', 
+                               title='Редактирование страницы книги', 
+                               form=form, page=page)
+        
+        
 @app.route('/book')
 def book_main():
     return render_template('book.html', title='Книга')
@@ -70,7 +113,7 @@ def users_main():
     db_sess = db_session.create_session()
     lst = []
     for elem in db_sess.query(Page).filter(Page.author == current_user.id):
-        lst.append(elem.header)
+        lst.append(elem)
     return render_template('users.html', title='Пользователи', pages=lst)
 
 
@@ -133,27 +176,46 @@ def create_page(page_type):
     db_sess = db_session.create_session()
     page = Page()
     page.author = current_user.id
-    page.type = 'edited'
+    page.type = page_type
+    page.edit_type = 'edited'
     db_sess.add(page)
     db_sess.commit()
-    return redirect('/users')
+    db_sess = db_session.create_session()
+    for elem in db_sess.query(Page).all():
+        page = elem
+    os.mkdir('static/img/book_pages/directory{}'.format(page.id))
+    json_page = {
+        'content': [
+            {
+                'header': 'Название 1',
+                'imgs': [],
+                'content': 'Текст 1',
+            }            
+        ]
+    }
+    with open('static/img/book_pages/directory{}/json_file.json'.format(
+        page.id), 
+              'w') as file:
+        json.dump(json_page, file)
+    page.json_page = 'static/img/book_pages/directory{}/json_file.json'.format(
+        page.id)    
+    db_sess.commit()    
+    return redirect('/edit_page/page_of_book${}'.format(page.id))
 
 
-@app.route('/edit_page/<page_type>/<page_id>', methods=['GET', 'POST'])
-@login_required
-def edit_page(page_type, page_id):
-    if page_type == 'page_of_book':
-        form = CreatePageForm()
-        if form.validate_on_submit():
-            return redirect('/users')
-        else:
-            return render_template('edit_page_of_book.html', 
-                               title='Новая страница книги', 
-                               form=form)
+'''def delete_page():
+    pass'''
+
+
+def clear_database():
+    db_sess = db_session.create_session()
+    db_sess.query(Page).delete()
+    db_sess.commit()
     
     
 def main():
     db_session.global_init("db/database.db")
+    # clear_database()
     app.run(port=8080, host='127.0.0.1')
 
 
