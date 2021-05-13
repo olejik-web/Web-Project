97d4bd095a07178343021d6ed692540c753670aa
olegator2004 <59178029+olejik-web@users.noreply.github.com>
2021-05-13 19:19:27 +0300

    Commit-13.05-19:19

--- main.py
+++ main.py
@@ -702,7 +702,8 @@ def clear_database():
 def main():
     db_session.global_init("db/database.db")
     # clear_database()
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
